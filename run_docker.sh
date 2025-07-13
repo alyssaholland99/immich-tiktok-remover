06241335406ad347d3927cd24500e3fc7509a1b9
Alyssa Holland <2011hollanda@gmail.com>
2025-07-14 00:00:26 +0100

    Made image with stop api call

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 python3 immich_tiktok_remover.py
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"immich_url":$IMMICH_URL,"immich_api_key":$IMMICH_API}' \
+  http://localhost:5000/stop
