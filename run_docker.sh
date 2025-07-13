5c305135e49d5b6e75abb9ffe1de7974666139c2
Alyssa Holland <2011hollanda@gmail.com>
2025-07-14 00:02:05 +0100

    exit 1

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,4 @@
 
 python3 immich_tiktok_remover.py
 
-curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"immich_url":$IMMICH_URL,"immich_api_key":$IMMICH_API}' \
-  http://localhost:5000/stop
+exit 1
