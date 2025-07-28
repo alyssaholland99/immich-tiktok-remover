e58e0bbb590e1f7e08e51a3ad1b520c95fb18ce7
Alyssa Holland <2011hollanda@gmail.com>
2025-07-29 00:17:16 +0100

    First lite drone run

---
+++ make_lite.sh
@@ -0,0 +1,3 @@
+sed -i -e 's/easyocr\=\=1\.7\.1/#\ Removed\ EasyOCR/g' requirements.txt
+sed -i -e 's/import\ easyocr/#\ Removed\ EasyOCR/g' src/image_verification.txt
+sed -i -e 's/from\ image_verification\ import\ processVideo\,\ verifyVideoNameAndDate/#\ Removed\ EasyOCR/g' src/immich_tiktok_remover.txt
