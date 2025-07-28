b93a36bc3217281c4ce0308239160caa0f8b73f1
Alyssa Holland <2011hollanda@gmail.com>
2025-07-29 00:32:07 +0100

    Removed redundant code

--- make_lite.sh
+++ make_lite.sh
@@ -1,5 +1,3 @@
-pwd
-ls
 sed -i -e 's/easyocr\=\=1\.7\.1/#\ Removed\ EasyOCR/g' requirements.txt
 sed -i -e 's/import\ easyocr/#\ Removed\ EasyOCR/g' src/image_verification.py
 sed -i -e 's/from\ image_verification\ import\ processVideo\,\ verifyVideoNameAndDate/#\ Removed\ EasyOCR/g' src/immich_tiktok_remover.py
