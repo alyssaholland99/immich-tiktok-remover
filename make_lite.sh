86ca0f57e46879e0ed306b531a92bca0a7d6c64f
Alyssa Holland <2011hollanda@gmail.com>
2025-07-29 02:38:41 +0100

    Drone dual pipeline test

--- make_lite.sh
+++ make_lite.sh
@@ -1,3 +1,5 @@
+cp /drone/srv /drone/src-lite
+cd /drone/src-lite
 sed -i -e 's/easyocr\=\=1\.7\.1/#\ Removed\ EasyOCR/g' requirements.txt
 sed -i -e 's/import\ easyocr/#\ Removed\ EasyOCR/g' src/image_verification.py
 sed -i -e 's/from\ image_verification\ import\ processVideo\,\ verifyVideoNameAndDate/#\ Removed\ EasyOCR/g' src/immich_tiktok_remover.py
