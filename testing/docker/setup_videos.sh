a0b970e4478d5b2446f473f729aa4662578c9106
alyssaholland99 <2011hollanda@gmail.com>
2025-08-17 18:36:29 -0700

    Removed build for testing

--- testing/docker/setup_videos.sh
+++ testing/docker/setup_videos.sh
@@ -8,11 +8,11 @@ docker ps
 docker cp ./testing/docker/tiktok_videos immich_server:/usr/src/app/videos
 docker cp ./testing/docker/non-tiktok_videos immich_server:/usr/src/app/videos
 
-sleep 10
+sleep 20
 
 docker exec immich_server bash -c "ls /usr/src/app/videos"
 
 # Upload video files to immich via the CLI
 
-docker exec immich_server bash -c "immich login http://immich_server:2283/api sHXdxnG2xoPNveGqJI8nZSlwTEMTFvILHqzCRFyfz4 && sleep 2 && immich upload --recursive /usr/src/app/videos/tiktok_videos"
-docker exec immich_server bash -c "immich login http://immich_server:2283/api sHXdxnG2xoPNveGqJI8nZSlwTEMTFvILHqzCRFyfz4 && sleep 2 && immich upload --recursive /usr/src/app/videos/non-tiktok_videos"
+docker exec immich_server bash -c "immich login http://immich_server:2283/api sHXdxnG2xoPNveGqJI8nZSlwTEMTFvILHqzCRFyfz4 && sleep 10 && immich upload --recursive /usr/src/app/videos/tiktok_videos"
+docker exec immich_server bash -c "immich login http://immich_server:2283/api sHXdxnG2xoPNveGqJI8nZSlwTEMTFvILHqzCRFyfz4 && sleep 10 && immich upload --recursive /usr/src/app/videos/non-tiktok_videos"
