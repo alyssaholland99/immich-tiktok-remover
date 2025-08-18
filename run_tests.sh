80310f168ead8d2b2ec2f3386ac9ceef91e4c82d
alyssaholland99 <2011hollanda@gmail.com>
2025-08-18 05:29:16 +0100

    Added a confirmation screen to warn people not to run in a live environment

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-sudo
-sudo cp docker-compose.yml docker-compose-backup.yml
-sudo ./testing/docker/setup_time.sh
-sudo ./testing/docker/setup_compose.sh
-sudo ./testing/docker/restore_db.sh
-sudo ./testing/docker/setup_videos.sh
-sudo ./testing/docker/run_tiktok_remover.sh
-sudo ./testing/docker/check_itr_output.sh
-sudo mv docker-compose-backup.yml docker-compose.yml
-sudo ./testing/docker/cleanup.sh 2>/dev/null 
+read -p "These tests should not be run in a live environment where you have Immich running. Continue? (Y/N) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo cp docker-compose.yml docker-compose-backup.yml
+    sudo ./testing/docker/setup_time.sh
+    sudo ./testing/docker/setup_compose.sh
+    sudo ./testing/docker/restore_db.sh
+    sudo ./testing/docker/setup_videos.sh
+    sudo ./testing/docker/run_tiktok_remover.sh
+    sudo ./testing/docker/check_itr_output.sh
+    sudo mv docker-compose-backup.yml docker-compose.yml
+    sudo ./testing/docker/cleanup.sh 2>/dev/null
+fi
